9a694030128a6e5469fb30d784558964df9be6d6
ren1el <renielxocampo@gmail.com>
2020-09-10 18:15:02 -0700

    Changed port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const config = require('./utils/config');
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+const PORT = process.env.PORT || config.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
